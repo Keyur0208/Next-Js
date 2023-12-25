3bcc69970f1234bbddc42e6ca621144cb8cdd2b6
keyur <keyurpansuriya88@outlook.com>
2023-12-25 18:59:03 +0530

    User Page All Comment

--- app/(page)/userfetch/page.js
+++ app/(page)/userfetch/page.js
@@ -1,31 +1,40 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-// export async function user_list() {
-//      let user_data = await fetch("http://localhost:3000/api/users");
-//     user_data = await user_data.json();
-//     return user_data;
+// // export async function user_list() {
+// //      let user_data = await fetch("http://localhost:3000/api/users");
+// //     user_data = await user_data.json();
+// //     return user_data;
+// // }
+
+
+// export default async function user() {
+
+//     // const users = await user_list();
+//     // console.log(users);
+
+//     return (
+//         <>
+//             <h1>User List</h1>
+//             {
+//                 users.map((item) => {
+
+//                     return (
+//                         <>
+//                             {/* <Link href={`userfetch/${item.id}`} style={{fontSize:'2rem'}} > {item.first_name}</Link><br></br> */}
+//                         </>
+//                     )
+//                 })
+//             }
+//         </>
+//     )
 // }
 
+import React from 'react'
 
-export default async function user() {
-
-    const users = await user_list();
-    console.log(users);
-
-    return (
-        <>
-            <h1>User List</h1>
-            {
-                users.map((item) => {
-
-                    return (
-                        <>
-                            {/* <Link href={`userfetch/${item.id}`} style={{fontSize:'2rem'}} > {item.first_name}</Link><br></br> */}
-                        </>
-                    )
-                })
-            }
-        </>
-    )
+const page = () => {
+  return (
+    <div>page</div>
+  )
 }
 
+export default page
