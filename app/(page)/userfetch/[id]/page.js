bb5d8169afe437e035784db8fe207fb5f265bf64
keyur <keyurpansuriya88@outlook.com>
2023-12-26 18:45:00 +0530

    Next_js in POST Method and Post Method Task and PUT Method

--- app/(page)/userfetch/[id]/page.js
+++ app/(page)/userfetch/[id]/page.js
@@ -1,33 +1,32 @@
-// import React from 'react'
-
-// // export async function user_list(id) {
-// //   // let user_data = await fetch(`http://localhost:3000/api/users/${id}`);
-// //   user_data = await user_data.json();
-// //   return user_data;
-// // }
-
-// export default async function user_id({params}) {
-
-//   // const users = await user_list(params.id);
-//   // console.log(users);
-
-//   return (
-//     <>
-//       <h1>User List</h1>
-//       <h3>id = {users.id}</h3>
-//       <h3>First Name ={users.first_name}</h3>
-//       <h3>Last  Name ={users.second_name}</h3>
-//     </>
-//   )
-// }
-
-
 import React from 'react'
 
-const page = () => {
+export async function user_list() {
+  let user_data = await fetch("http://localhost:3000/api/users");
+  user_data = await user_data.json();
+  return user_data;
+}
+
+export default async function user_id(props) {
+
+  const  getdata = user_list();
+  const users =  await getdata;
+  const currentId = props.params.id;
+  const userdata = users[currentId-1];
+  console.log(userdata);
+
   return (
-    <div>page</div>
+    <>
+      <h1>User List</h1>
+      <h3>id = {userdata.id}</h3>
+      <h3>First Name ={userdata.first_name}</h3>
+      <h3>Last  Name ={userdata.last_name}</h3>
+      <h3>Age ={userdata.age}</h3>
+      <h3>City ={userdata.city}</h3>
+    </>
   )
 }
 
-export default page
+
+
+
+
